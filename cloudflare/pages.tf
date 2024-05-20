23d95fa445f8ea31ebd525feb80b41fc4d78118d
quanianitis <ichan@moneylion.com>
2024-05-20 09:05:44 +0800

    feat: start of tailwind css blog for quanianitis.com

--- cloudflare/pages.tf
+++ cloudflare/pages.tf
@@ -3,23 +3,20 @@ module "quanianitis" {
   account_id = local.cloudflare_account_id
 
   project_name      = "quanianitis"
-  production_branch = "main"
+  production_branch = "alt-main"
 
-  preview_environment_variables = {
-    HUGO_VERSION = "0.120.0"
-  }
-  production_environment_variables = {
-    HUGO_VERSION = "0.120.0"
-  }
+  preview_environment_variables    = {}
+  production_environment_variables = {}
 
   preview_compatibility_date    = "2024-04-28"
   production_compatibility_date = "2024-04-28"
+  compatibility_flags           = ["nodejs_compat"]
 
   domain = ["quanianitis.com"]
 
   build_config = {
     build_caching   = true
-    build_command   = "hugo --config main/config.yaml"
-    destination_dir = "public"
+    build_command   = "npx @cloudflare/next-on-pages@1"
+    destination_dir = "/.vercel/output/static"
   }
 }
